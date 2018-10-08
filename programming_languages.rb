5ea53960a50a9c1a1c0627638495374f5317f410
Jacob Menashi <jbmenashi@gmail.com>
2018-10-08 14:44:09 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,11 +2,15 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language|
     language.each do |language, type|
+      # need to iterate twice because that separates the language from its type information
       if new_hash[language]
+        # if the new hash already has a key called language
         new_hash[language][:style] << style
+        # then just push anymore styles to the style area
       else
         new_hash[language] = type
         new_hash[language][:style] = [style]
+        # else you have create the new keys/values for the new hash
       end
     end
   end
