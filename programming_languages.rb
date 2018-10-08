eba4929dbeced23768a3138310eb1345d6bd2057
Jacob Menashi <jbmenashi@gmail.com>
2018-10-08 14:29:02 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,8 +3,7 @@ require 'pry'
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language|
-    new_hash[language] = :style
-    new_hash[language] = []
+    new_hash[language] = :type
   end
   binding.pry
   
